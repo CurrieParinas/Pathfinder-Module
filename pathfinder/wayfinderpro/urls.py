0a2dd98cf149df62cfb5362ca7cf751aef50223e
msrosales <msrosales@up.edu.ph>
2024-05-18 17:57:37 +0800

    separate page wayfinder

--- pathfinder/wayfinderpro/urls.py
+++ pathfinder/wayfinderpro/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='wayfinderpro-home'),
+    path('finder/', views.finder, name='wayfinderpro-room-finder'),
+    path('finder/displayPath/', views.displayPath, name='wayfinderpro-display-path'),
+
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
